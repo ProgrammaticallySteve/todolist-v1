26a13d28c6aa585586645f07987301f287c2e253
Stephen Harvey <itsteveharvey@gmail.com>
2022-10-15 11:17:53 +1300

    Created ToDo list functionality

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get("/", function(req, res) {
-  res.send("Hello");
+
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-GB", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+
 });
 
 
